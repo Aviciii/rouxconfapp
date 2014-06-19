38d5c04d03c66594d941d7f670413d60a79f0293
Aviciii <khalifa.211@gmail.com>
2014-06-19 19:14:46 +0500

    basic  scaffold for site complete

---
+++ privacypolicy.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Roux Academy Conferenece 2013 -- Privacy Policy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,700italic,300,300italic,400italic,700' rel='stylesheet' type='text/css'>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyle.css" rel="stylesheet">
+  </head>
+  <body id="privacypolicy">  
+    
+    <section class="container">
+      <div class="content row">
+        <section class="main col col-lg-8">
+          
+        </section> <!-- main -->
+        <section class="sidebar col col-lg-4">
+          
+        </section> <!-- sidebar -->
+      </div> <!-- content -->
+    </section> <!-- container -->
+    
+
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
+</html>
